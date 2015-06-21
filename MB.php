b2c4854390821a3d9dc521a85d898c82ac33a352
gkerickson <gkerickson@wisc.edu>
2015-06-21 01:19:08 -0500

    Added second page

--- MB.php
+++ MB.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>MoodBoost</title>
+		<title>Mood-Boost</title>
 		<link href="bootstrap-3.3.5-dist/css/bootstrap.css" type="test/css" rel="stylesheet">
 		<link href="MB.css" type="text/css" rel="stylesheet" >
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
