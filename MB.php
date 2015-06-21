d0ca8380d3f61ab0b83e29e5c49af01b8e731430
gkerickson <gkerickson@wisc.edu>
2015-06-20 21:03:39 -0500

    Working stuff!

--- MB.php
+++ MB.php
@@ -8,15 +8,15 @@
 		<script language="JavaScript" src="MB.js"></script>
 	</head>
 	<body>
-		<!--?php require 'db.php'; ?-->
-		<!--select id="mdlst"-->
-			<!--?php
+		<?php require 'db.php'; ?>
+		<select id="mdlst">
+			<?php
 				$mood = 0;
-                foreach ($pdo->query("SELECT name FROM Mood") as $mood) {
-                	echo("<option value=\"$mood\">$mood</option>");
-                }
-			?-->
-		<!--/select-->
+		                foreach ($pdo->query("SELECT name FROM Mood") as $mood) {
+                			echo("<option value=\"$mood\">$mood</option>");
+               			 }
+			?>
+		</select>
 		<h1> How are you feeling today?</h1>
 		<?php
 			$emotions = array("Sad","Happy","Shitty","Horny");
--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "dbuser";
+$password = "123";
 $dbname = "MBDB";
 try{$pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", "$username", "$password");}
 catch(PDOException $e){
