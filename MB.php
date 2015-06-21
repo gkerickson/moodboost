91f2b5e824a8478975629641142c09f5c7d2e347
Galen <gkerickson@wisc.edu>
2015-06-20 21:30:03 -0500

    Update MB.php

--- MB.php
+++ MB.php
@@ -22,15 +22,6 @@
                 
 			?>
 		</select>
-		<?php require 'db.php'; ?>
-		<select id="mdlst">
-			<?php
-				$mood = 0;
-		                foreach ($pdo->query("SELECT name FROM Mood") as $mood) {
-                			echo("<option value=\"$mood\">$mood</option>");
-               			 }
-			?>
-		</select>
 		<h1> How are you feeling today?</h1>
 		<?php
 			$emotions = array("Sad","Happy","Shitty","Horny");
