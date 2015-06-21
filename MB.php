b665f33372e329a5b6dab5002701f5fec2226132
Jared H <hoffmaj2@rose-hulman.edu>
2015-06-20 22:04:24 -0400

    diesiraediesilla
    solvetsicluminfavilla

--- MB.php
+++ MB.php
@@ -2,27 +2,25 @@
 <html>
 	<head>
 		<title>MoodBoost</title>
-		<link href="./bootstrap-3.3.5-dist/css/bootstrap.css" type="test/css" rel="stylesheet">
-		<link href="MB.css" type="text/css" rel="stylesheet">
+		<!--link href="MB.css" type="text/css" rel="stylesheet" /-->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script language="JavaScript" src="MB.js"></script>
 	</head>
+
 	<body>
-		<!--?php require 'db.php'; ?-->
-		<!--select id="mdlst"-->
-			<!--?php
-				$mood = 0;
+	   <?php require 'db.php'; ?>
+		<select id="mdlst">
+			<?php
+			 //$mood = 0;
                 foreach ($pdo->query("SELECT name FROM Mood") as $mood) {
-                	echo("<option value=\"$mood\">$mood</option>");
+                    echo("<option value=\"");
+                    echo($mood[0]);
+                    echo("\">");
+                    echo($mood[0]);
+                    echo("</option>");
                 }
-			?-->
-		<!--/select-->
-		<h1> How are you feeling today?</h1>
-		<?php
-			$emotions = array("Sad","Happy","Shitty","Horny");
-			foreach ($emotions as $emotion){
-				echo "<button class=\"btn btn-default\" type=\"submit\">$emotion</button>";
-			}
-		?>
+                
+			?>
+		</select>
 	</body>
 </html>
