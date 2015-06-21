8c90269c103193f811b102c451274bf8c6998071
gkerickson <gkerickson@wisc.edu>
2015-06-20 22:03:34 -0500

    All before change

--- MB.php
+++ MB.php
@@ -7,27 +7,24 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script language="JavaScript" src="MB.js"></script>
 	</head>
-
 	<body>
-	   <?php require 'db.php'; ?>
-		<select id="mdlst">
-			<?php
-			 //$mood = 0;
-                foreach ($pdo->query("SELECT name FROM Mood") as $mood) {
-                    echo("<option value=\"");
-                    echo($mood[0]);
-                    echo("\">");
-                    echo($mood[0]);
-                    echo("</option>");
-                }
-                
-			?>
-		</select>
+	   <?php require 'db.php';?>
+		<!--select id="mdlst"-->
+			<!--?php
+//                foreach ($pdo->query("SELECT name FROM Mood") as $mood) {
+//                    echo("<option value=\"");
+//                    echo($mood[0]);
+//                    echo("\">");
+//                    echo($mood[0]);
+//                    echo("</option>");
+//                }
+			?-->
+		<!--/select-->
 		<h1> How are you feeling today?</h1>
 		<?php
 			$emotions = array("Sad","Happy","Shitty","Horny");
-			foreach ($emotions as $emotion){
-				echo "<button class=\"btn btn-default\" type=\"submit\">$emotion</button>";
+			foreach ($pdo->query("SELECT name FROM Mood") as $mood){
+				echo "<button class=\"btn btn-default\" type=\"submit\">$mood[0]</button>";
 			}
 		?>
 	</body>
