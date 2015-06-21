40d84c96e9e7b64992c16d716473a3101c7a584b
gkerickson <gkerickson@wisc.edu>
2015-06-20 20:27:16 -0500

    Added styles n stuff

--- MB.php
+++ MB.php
@@ -2,11 +2,11 @@
 <html>
 	<head>
 		<title>MoodBoost</title>
-		<!--link href="MB.css" type="text/css" rel="stylesheet" /-->
+		<link href="MB.css" type="text/css" rel="stylesheet">
+		<link href="./bootstrap-3.3.5-dist/css" type="test/css" rel="stylesheet">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script language="JavaScript" src="MB.js"></script>
 	</head>
-
 	<body>
 		<!--?php require 'db.php'; ?-->
 		<!--select id="mdlst"-->
@@ -14,7 +14,7 @@
 				$mood = 0;
                 foreach ($pdo->query("SELECT name FROM Mood") as $mood) {
                 	echo("<option value=\"$mood\">$mood</option>");
-                }               
+                }
 			?-->
 		<!--/select-->
 		<h1> How are you feeling today?</h1>
