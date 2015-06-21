3057bf7938d01112bfd17e8bb10f4655ae646903
gkerickson <gkerickson@wisc.edu>
2015-06-20 19:39:10 -0500

    Working Site

--- MB.php
+++ MB.php
@@ -8,15 +8,21 @@
 	</head>
 
 	<body>
-	   <?php require 'database.php'; ?>
-		<select id="mdlst">
-			<?php
-			 //$mood = 0;
+		<!--?php require 'db.php'; ?-->
+		<!--select id="mdlst"-->
+			<!--?php
+				$mood = 0;
                 foreach ($pdo->query("SELECT name FROM Mood") as $mood) {
-                    echo("<option value=\"$mood\">$mood</option>");
-                }
-                
-			?>
-		</select>
+                	echo("<option value=\"$mood\">$mood</option>");
+                }               
+			?-->
+		<!--/select-->
+		<h1> How are you feeling today?</h1>
+		<?php
+			$emotions = array("Sad","Happy","Shitty","Horny");
+			foreach ($emotions as $emotion){
+				echo "<button type=\"button\">$emotion</button>";
+			}
+		?>
 	</body>
 </html>
