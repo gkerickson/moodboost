166985b9685691211f5441c1a592af3d922e2a93
gkerickson <gkerickson@wisc.edu>
2015-06-20 20:35:08 -0500

    Bootstrap Works!

--- MB.php
+++ MB.php
@@ -3,7 +3,7 @@
 	<head>
 		<title>MoodBoost</title>
 		<link href="MB.css" type="text/css" rel="stylesheet">
-		<link href="./bootstrap-3.3.5-dist/css" type="test/css" rel="stylesheet">
+		<link href="./bootstrap-3.3.5-dist/css/bootstrap.css" type="test/css" rel="stylesheet">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script language="JavaScript" src="MB.js"></script>
 	</head>
@@ -21,7 +21,7 @@
 		<?php
 			$emotions = array("Sad","Happy","Shitty","Horny");
 			foreach ($emotions as $emotion){
-				echo "<button type=\"button\">$emotion</button>";
+				echo "<button class=\"btn btn-default\" type=\"submit\">$emotion</button>";
 			}
 		?>
 	</body>
